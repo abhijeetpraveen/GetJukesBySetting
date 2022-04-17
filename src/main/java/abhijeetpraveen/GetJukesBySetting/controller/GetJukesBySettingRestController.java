a672f1abd038d0da7600962686c7678929179e9b
Abhijeet Praveen <abhijeetpraveen@gmail.com>
2022-04-17 00:16:35 -0400

    commented rest controller

--- src/main/java/abhijeetpraveen/GetJukesBySetting/controller/GetJukesBySettingRestController.java
+++ src/main/java/abhijeetpraveen/GetJukesBySetting/controller/GetJukesBySettingRestController.java
@@ -12,10 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GetJukesBySettingRestController {
 
+
     @Autowired
     GetJukesBySettingService service;
 
 
+    /**
+     * The RESTful Service Get Endpoint that calls the method from the service class to get all supported jukes
+     * @param id the setting ID for which the user wishes to find all compatible jukes
+     * @param model an optional parameter where the user can filter jukes by their model
+     * @param offset an optional parameter where the user can indicate at what index the page should start
+     * @param limit an optional parameter where the user can indicate the page size
+     * @return a String response entity corresponding to the paginated list or any error thrown by the error
+     */
     @GetMapping(value = "/supportedJukes")
     public ResponseEntity<String> getJukesBySetting(@RequestParam(value = "id", defaultValue = "") String id,
                                                @RequestParam(value = "model", defaultValue = "") String model,
